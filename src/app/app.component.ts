1737b9c2f606031e3d77660629643eb840476516
Netipong Sanklar <jj_sanklar@hotmail.co.th>
2026-01-28 01:38:53 +0700

    update data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component} from '@angular/core';
 
 export class AppComponent{ 
 
-  birthDate: Date = new Date('2001-08-31T14:29:00');  // Replace with the actual birth date
+  birthDate: Date = new Date('2001-08-31T14:29:00');
   realTimeAge: string = '';
 
   constructor() {
@@ -23,12 +23,12 @@ export class AppComponent{
       const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
       const months = Math.floor((diff % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
       const days = Math.floor((diff % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
-      const hours = now.getHours();
-      const minutes = now.getMinutes();
+      const hours = now.getHours().toFixed(0).padStart(2, '0');
+      const minutes = now.getMinutes().toFixed(0).padStart(2, '0');
       const seconds = now.getSeconds();
       
-      this.realTimeAge = `${years} years, ${months} months, ${days} days, ${hours}:${minutes}:${seconds}`;
-
+      // this.realTimeAge = `${years} years, ${months} months, ${days} days, ${hours}:${minutes}:${seconds}`;
+      this.realTimeAge = `${years}.${months} years`;
   }
 
 }
