32737c09af6b98215a90bcd86b0d1fa5a5a91c9c
Netipong Sanklar <jj_sanklar@hotmail.co.th>
2026-02-01 04:36:48 +0700

    darkmode by time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component} from '@angular/core';
+import { Component, Renderer2, Inject, PLATFORM_ID } from '@angular/core'; // Import Renderer2
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,40 @@ export class AppComponent{
 
   birthDate: Date = new Date('2001-08-31T14:29:00');
   realTimeAge: string = '';
+  isDarkMode: boolean = false; 
 
-  constructor() {
+  // Inject Renderer2
+  constructor(private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {
     this.calculateRealTimeAge();
+    this.checkTimeForDarkMode(); 
+  }
+
+  checkTimeForDarkMode() {
+    const hour = new Date().getHours();
+    
+    // Check if we are in the browser to access document/body safely
+    if (isPlatformBrowser(this.platformId)) {
+        if (hour >= 18 || hour < 6) {
+            this.isDarkMode = true;
+            // Add class to body
+            this.renderer.addClass(document.body, 'dark-mode-body');
+        } else {
+            this.isDarkMode = false;
+            // Remove class from body (optional if default is false)
+            this.renderer.removeClass(document.body, 'dark-mode-body');
+        }
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.isDarkMode) {
+        this.renderer.addClass(document.body, 'dark-mode-body');
+      } else {
+        this.renderer.removeClass(document.body, 'dark-mode-body');
+      }
+    }
   }
 
   calculateRealTimeAge() {
